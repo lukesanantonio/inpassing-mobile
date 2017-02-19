57f5bc22d4ecb720452253f74562b25e7eb42275
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2017-02-18 22:30:38 -0500

    Add basic login / signup screen

---
+++ app/auth.js
@@ -0,0 +1,119 @@
+import React, {Component, PropTypes} from 'react';
+import {View, Text, TextInput, Button} from 'react-native';
+
+export class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput placeholder='Email'
+                   onChangeText={(text) => { this.setState({email: text})}}
+                   value={this.state.email}
+                   keyboardType='email-address'/>
+        <TextInput placeholder='Password'
+                   onChangeText={(text) => { this.setState({password: text})}}
+                   value={this.state.password}
+                   secureTextEntry={true}/>
+        <Button title='Login'
+                onPress={() => { this.props.onLogin(this.state.email,
+                                                    this.state.password) } } />
+        <Text>Need an account?</Text>
+        <Button title='Sign up'
+                onPress={this.props.onSwitch} />
+      </View>
+    );
+  }
+}
+
+Login.propTypes = {
+  onLogin: PropTypes.func.isRequired,
+  onSwitch: PropTypes.func.isRequired
+}
+
+export class Signup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput placeholder='First name'
+                   onChangeText={(text) => { this.setState({firstName: text})}}
+                   value={this.state.firstName} />
+        <TextInput placeholder='Last name'
+                   onChangeText={(text) => { this.setState({lastName: text})}}
+                   value={this.state.lastName} />
+        <TextInput placeholder='Email'
+                   onChangeText={(text) => { this.setState({email: text})}}
+                   value={this.state.email}
+                   keyboardType='email-address'/>
+        <TextInput placeholder='Password'
+                   onChangeText={(text) => { this.setState({password: text})}}
+                   value={this.state.password}
+                   secureTextEntry={true}/>
+        <Button title='Sign up'
+                onPress={() => { this.props.onSignup(this.state.firstName,
+                                                     this.state.lastName,
+                                                     this.state.email,
+                                                     this.state.password) } } />
+        <Text>Already have an account?</Text>
+        <Button title='Login'
+                onPress={this.props.onSwitch} />
+      </View>
+    );
+  }
+}
+
+Signup.propTypes = {
+  onSignup: PropTypes.func.isRequired,
+  onSwitch: PropTypes.func.isRequired,
+}
+
+export class Auth extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mode: Login
+    };
+  }
+
+  _onLogin(email, password) {
+      // We need to parse that data
+      alert(email);
+  }
+
+  _onSignup(firstName, lastName, email, password) {
+      // Sign up
+      alert(firstName);
+  }
+
+  _onSwitch(comp) {
+      if (comp.state.mode === Login) {
+        comp.setState({mode: Signup});
+      } else if (comp.state.mode === Signup) {
+        comp.setState({mode: Login});
+      }
+  }
+
+  render() {
+    return <this.state.mode onLogin={this._onLogin} onSignup={this._onSignup}
+                            onSwitch={() => {this._onSwitch(this)} } />
+  }
+}

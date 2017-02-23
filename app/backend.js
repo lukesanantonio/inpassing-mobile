fcd4eac81222ef90923619323bc9910c226dc38c
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2017-02-22 20:43:00 -0500

    Clean up resource / promises in InPassingCursor class
    Adds support for caching orgs by ID and paves the way for caching users with
    their ID as well, although currently the me user object is the only thing
    cached (it uses ID 0).

--- app/backend.js
+++ app/backend.js
@@ -19,26 +19,97 @@ function makeBearerAuth(jwt) {
   return 'Bearer ' + jwt;
 }
 
+export class Org {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  static fromApiObj(obj) {
+    return new Org(obj.id, obj.name);
+  }
+}
+
+export class Pass {
+  constructor(id, orgId, ownerId, stateId, spotNum) {
+    this.id = id;
+    this.orgId = orgId;
+    this.ownerId = ownerId;
+    this.stateId = stateId;
+    this.spotNum = spotNum;
+  }
+
+  static fromApiObj(obj) {
+    return new Pass(obj.id, obj.org_id, obj.owner_id, obj.state_id,
+                    obj.spot_num);
+  }
+}
+
 export class User {
-  constructor(id, firstName, lastName, email) {
+  constructor(id, firstName, lastName, email, passes) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
+    this.passes = passes;
+  }
+  static fromApiObj(obj) {
+    return new User(obj.id, obj.first_name, obj.last_name, obj.email,
+                    obj.passes.map(Pass.fromApiObj))
+  }
+}
+
+class ResourceCache {
+  constructor(func) {
+    this.promiseCache = {}
+    this.resourceCache = {}
+    this.func = func;
+  }
+
+  invalidate(id) {
+    this.promiseCache[id] = undefined;
+    this.resourceCache[id] = undefined;
+  }
+
+  getResource(id) {
+    if (id in this.resourceCache) {
+      // The resource has already been retrieved.
+      return Promise.resolve(this.resourceCache[id]);
+    } else if (id in this.promiseCache) {
+      // The resource is currently being retrieved.
+      return this.promiseCache[id]
+    } else {
+      // Retrieve the resource now
+      var promise = this.func(id);
+
+      // Take note of the promise
+      this.promiseCache[id] = promise;
+
+      promise.then((val) => {
+        // We found the resource, add it to our resource cache.
+        this.resourceCache[id] = val;
+      });
+
+      return promise;
+    }
   }
 }
 
 export class InPassingCursor {
   constructor(token) {
     this.token = token;
-    this.cachedMe = null;
+    this.userCache = new ResourceCache(this._getUser.bind(this));
+    this.orgCache = new ResourceCache(this._getOrgById.bind(this));
   }
 
-  async me() {
-    // If we already know who we are return that.
-    if (this.cachedMe !== null) return this.cachedMe;
+  async _getUser(id) {
+    if (id !== 0) {
+      // We need to query a specific user, but we don't support that yet.
+      throw ('Querying arbitrary users is not implemented yet. ' +
+             'Use ID 0 for me object');
+    }
 
-    // Figure out who we are authenticated as.
+    // Query me
     var res = await fetch(makeUrl(ME_ENDPOINT), {
       headers: {'Authorization': makeBearerAuth(this.token)}
     });
@@ -52,9 +123,26 @@ export class InPassingCursor {
       // Bad user
       return null;
     }
-    this.cachedMe = new User(usr_obj.id, usr_obj.first_name, usr_obj.last_name,
-                             usr_obj.email);
-    return this.cachedMe;
+    return User.fromApiObj(usr_obj);
+  }
+
+  async me() {
+    // User 0 is by convention the me user.
+    return this.userCache.getResource(0);
+  }
+
+  async _getOrgById(orgId) {
+    var res = await fetch(makeUrl('orgs/' + orgId));
+    var org_obj = await res.json();
+    if(org_obj.id === undefined) {
+      return null;
+    }
+    return Org.fromApiObj(org_obj);
+  }
+
+  getOrgById(orgId) {
+    // Returns a promise to the resource
+    return this.orgCache.getResource(orgId);
   }
 }
 

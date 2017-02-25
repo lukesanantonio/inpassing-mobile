cd8c9557dca89a1901b2a2cccbab7c511aa1fe3e
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2017-02-25 00:06:26 -0500

    Support text next to the arrow in ClickCard
    The layout is more robust now, and just works without the weird 15:1 flex ratio.

--- app/views/ClickCard.js
+++ app/views/ClickCard.js
@@ -7,7 +7,7 @@
 
 import React, {Component} from 'react';
 
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 
 import {
   Card, CardItem, Body, Right, Icon
@@ -15,28 +15,38 @@ import {
 
 const styles = StyleSheet.create({
   cardBody: {
-    flex: 15,
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
   },
-  cardIcon: {
-    flex: 1,
-  }
+  cardRight: {
+    flex: 0,
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
+  },
 });
 
 const style_cardBody = StyleSheet.flatten(styles.cardBody);
-const style_cardIcon = StyleSheet.flatten(styles.cardIcon);
+const style_cardRight = StyleSheet.flatten(styles.cardRight);
 
 export default class ClickCard extends Component {
   render() {
+    var arrowText = null;
+    if(this.props.arrowText) {
+      // That space after the {...} is intentional. It adds some space between
+      // the text and the icon.
+      arrowText = <Text>{this.props.arrowText} </Text>;
+    }
+
     return (
       <Card style={this.props.style}>
         <CardItem header button onPress={this.props.onPress}>
           <Body style={style_cardBody}>
             {this.props.children}
           </Body>
-          <Right style={style_cardIcon}>
+          <Right style={style_cardRight}>
             <Icon name="arrow-forward"/>
+            {arrowText}
           </Right>
         </CardItem>
       </Card>
